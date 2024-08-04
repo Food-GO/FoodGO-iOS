cbbd930f34cb03096d074b5cc4e13344e37929cc
keem-hyun <hw813@naver.com>
2024-08-04 16:24:10 +0900

    fix designsystem and add assets

--- YoriJori/DesignSystem/YorijoriButton.swift
+++ YoriJori/DesignSystem/YorijoriButton.swift
@@ -9,10 +9,12 @@ import UIKit
 
 final class YorijoriButton: UIButton {
     // MARK: - Property
-    private var buttonConfig = UIButton.Configuration.borderedTinted()
+    private var buttonConfig = UIButton.Configuration.filled()
     private var bgColor: UIColor
     private var textColor: UIColor
     private var borderColor: UIColor
+    private var selectedBorderColor: UIColor
+    
     
     var text: String = "" {
         didSet {
@@ -20,41 +22,49 @@ final class YorijoriButton: UIButton {
         }
     }
     
+    override var isSelected: Bool {
+        didSet {
+            updateAppearance()
+        }
+    }
+    
     // MARK: - Initializer
-    init(bgColor: UIColor, textColor: UIColor, borderColor: UIColor) {
+    init(bgColor: UIColor, textColor: UIColor, borderColor: UIColor, selectedBorderColor: UIColor) {
         self.bgColor = bgColor
         self.textColor = textColor
         self.borderColor = borderColor
+        self.selectedBorderColor = selectedBorderColor
         super.init(frame: .zero)
-        setTitle()
-        setBackgroundColor()
-        setBorderColor()
-        self.configuration = buttonConfig
+        setupInitialState()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // MARK: - InitUI
+    private func setupInitialState() {
+        buttonConfig.background.backgroundColor = bgColor
+        buttonConfig.baseForegroundColor = textColor
+        buttonConfig.background.strokeColor = borderColor
+        buttonConfig.background.strokeWidth = 1
+        setTitle()
+        updateAppearance()
+        self.configuration = buttonConfig
+    }
     
     private func setTitle() {
         var titleContainer = AttributeContainer()
         titleContainer.font = DesignSystemFont.subTitle1
         titleContainer.foregroundColor = textColor
         buttonConfig.titleAlignment = .center
-        
         buttonConfig.attributedTitle = AttributedString(text, attributes: titleContainer)
         self.configuration = buttonConfig
     }
     
-    private func setBorderColor() {
-        buttonConfig.background.strokeColor = borderColor
-        buttonConfig.background.strokeWidth = 1
-    }
-    
-    private func setBackgroundColor() {
-        buttonConfig.baseBackgroundColor = bgColor
+    private func updateAppearance() {
+        buttonConfig.background.strokeColor = isSelected ? selectedBorderColor : borderColor
+        buttonConfig.baseForegroundColor = textColor
+        self.configuration = buttonConfig
     }
     
 }
