05f21cc370ac4907884749b5409df8e96791b184
keem-hyun <hw813@naver.com>
2024-08-17 16:27:50 +0900

    [ADD] Label Extension - 특정 문자 컬러 변경

--- YoriJori/Resources/Extension/UILabel+Ext.swift
+++ YoriJori/Resources/Extension/UILabel+Ext.swift
@@ -5,4 +5,14 @@
 //  Created by 김강현 on 8/17/24.
 //
 
-import Foundation
+import UIKit
+
+extension UILabel {
+    func asColor(targetString: String, color: UIColor) {
+        let fullText = text ?? ""
+        let attributedString = NSMutableAttributedString(string: fullText)
+        let range = (fullText as NSString).range(of: targetString)
+        attributedString.addAttribute(.foregroundColor, value: color, range: range)
+        attributedText = attributedString
+    }
+}
