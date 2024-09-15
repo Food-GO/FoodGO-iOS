290e29e2cdef3766848ec9c3a86d9a384886a965
keem-hyun <ddiamo997@gmail.com>
2024-09-15 09:25:14 +0900

    [ADD] 이미지 추가 및 디자인시스템 폰트

--- YoriJori/DesignSystem/DesignSystemFont.swift
+++ YoriJori/DesignSystem/DesignSystemFont.swift
@@ -24,7 +24,9 @@ public enum DesignSystemFont {
     static let caption2 = UIFont.suit(.regular, size: 12)
     
     static let extraBold20 = UIFont.suit(.extraBold, size: 20)
+    static let extraBold16 = UIFont.suit(.extraBold, size: 16)
     
+    static let bold20 = UIFont.suit(.bold, size: 20)
     static let bold18 = UIFont.suit(.bold, size: 18)
     static let bold16 = UIFont.suit(.bold, size: 16)
     static let bold14 = UIFont.suit(.bold, size: 14)
@@ -33,9 +35,13 @@ public enum DesignSystemFont {
     static let semibold18 = UIFont.suit(.semiBold, size: 18)
     static let semibold16 = UIFont.suit(.semiBold, size: 16)
     static let semibold15 = UIFont.suit(.semiBold, size: 15)
+    static let semibold12 = UIFont.suit(.semiBold, size: 12)
     
     static let medium20 = UIFont.suit(.medium, size: 20)
     static let medium14 = UIFont.suit(.medium, size: 14)
+    static let medium12 = UIFont.suit(.medium, size: 12)
+    
+    
     static let regular14 = UIFont.suit(.regular, size: 14)
     static let regular15 = UIFont.suit(.regular, size: 15)
 }
