64650f68cfcf31adba41d108d1e01f908a3884b5
keem-hyun <ddiamo997@gmail.com>
2024-09-15 09:28:03 +0900

    [REFACT] 음식 추천 탭

--- YoriJori/Presentation/RecommendFood/RecommendFoodTapViewController.swift
+++ YoriJori/Presentation/RecommendFood/RecommendFoodTapViewController.swift
@@ -13,26 +13,65 @@ class RecommendFoodTapViewController: UIViewController {
     
     private let disposeBag = DisposeBag()
     
+    private let scrollView = UIScrollView().then {
+        $0.isScrollEnabled = true
+    }
+    
+    private let contentView = UIView().then {
+        $0.isUserInteractionEnabled = true
+    }
+    
     private let topBackgroundView = UIView().then {
         $0.backgroundColor = DesignSystemColor.yorijoriPink
         $0.layer.cornerRadius = 12
         $0.layer.masksToBounds = true
     }
     
+//    private let userTasteLabel = UILabel().then {
+//        $0.text = "요리조리님은,\n에너지 운동가"
+//        $0.numberOfLines = 0
+//        $0.font = DesignSystemFont.extraBold16
+//        $0.textColor = DesignSystemColor.white
+//        $0.textAlignment = .center
+//        $0.asColor(targetString: "에너지 운동가", color: UIColor.init(hex: "#B92100"))
+//    }
+    
+    private let nameLabel = UILabel().then {
+        $0.text = "요리조리님은,"
+        $0.font = DesignSystemFont.extraBold20
+        $0.textColor = DesignSystemColor.gray100
+    }
+    
+    private let typeLabel = UILabel().then {
+        $0.font = UIFont.suit(.bold, size: 28)
+        $0.text = "에너지 운동가"
+        $0.textColor = UIColor.init(hex: "#B92100")
+    }
+    
+    private let typeUnderLine = UIImageView().then {
+        $0.image = UIImage(named: "type_underLine")
+    }
+    
     private let profileImage = UIImageView().then {
         $0.image = UIImage(named: "energe_character")
     }
     
-    private let userTasteLabel = UILabel().then {
-        $0.text = "이00님의 음식 취향은\n어떤 취향 일까요?"
-        $0.numberOfLines = 0
-        $0.font = DesignSystemFont.semibold18
-        $0.textColor = DesignSystemColor.white
-        $0.textAlignment = .center
-    }
+//    private let userTasteLabel = UILabel().then {
+//        $0.text = "요리조리님의 음식 취향은\n어떤 취향 일까요?"
+//        $0.numberOfLines = 0
+//        $0.font = DesignSystemFont.semibold18
+//        $0.textColor = DesignSystemColor.white
+//        $0.textAlignment = .center
+//    }
     
-    private let foodTasteTestButton = YorijoriFilledButton(bgColor: DesignSystemColor.white, textColor: DesignSystemColor.yorijoriPink).then {
-        $0.text = "테스트 하러가기"
+//    private let foodTasteTestButton = YorijoriFilledButton(bgColor: DesignSystemColor.white, textColor: DesignSystemColor.yorijoriPink).then {
+//        $0.text = "테스트 하러가기"
+//    }
+    
+    private let reTestButton = YorijoriFilledButton(bgColor: DesignSystemColor.gray100, textColor: DesignSystemColor.gray600).then {
+        $0.text = "테스트 다시하기"
+        $0.semanticContentAttribute = .forceRightToLeft
+        $0.setImage(UIImage(named: "return"), for: .normal)
     }
     
     private let grayView = UIView().then {
@@ -45,7 +84,14 @@ class RecommendFoodTapViewController: UIViewController {
         $0.textColor = DesignSystemColor.gray900
     }
     
-    private let registerView = FoodRegisterView()
+//    private let registerView = FoodRegisterView()
+    
+    private lazy var myIngredientsView = UIImageView().then {
+        $0.image = UIImage(named: "my_ingredients")
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.moveToFoodRegister))
+        $0.isUserInteractionEnabled = true
+        $0.addGestureRecognizer(tapGesture)
+    }
     
     private let foodRecommendButton = YorijoriFilledButton(bgColor: DesignSystemColor.yorijoriPink, textColor: DesignSystemColor.white).then {
         $0.text = "음식 추천받기"
@@ -54,7 +100,7 @@ class RecommendFoodTapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = DesignSystemColor.gray150
+        self.view.backgroundColor = DesignSystemColor.yorijoriPink
         
         setUI()
         bind()
@@ -64,36 +110,90 @@ class RecommendFoodTapViewController: UIViewController {
         super.viewWillAppear(animated)
         
         self.tabBarController?.tabBar.isHidden = false
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.navigationController?.navigationBar.isHidden = false
     }
     
     private func setUI() {
-        [topBackgroundView, grayView, myGradientLabel, registerView, foodRecommendButton].forEach({self.view.addSubview($0)})
-        [profileImage, userTasteLabel, foodTasteTestButton].forEach({self.topBackgroundView.addSubview($0)})
+        self.scrollView.backgroundColor = DesignSystemColor.white
+        self.contentView.backgroundColor = DesignSystemColor.white
+        self.view.addSubview(scrollView)
+        self.scrollView.addSubview(contentView)
+        
+        scrollView.snp.makeConstraints({
+            $0.edges.equalTo(self.view.safeAreaLayoutGuide)
+        })
+        
+        contentView.snp.makeConstraints({
+            $0.top.bottom.leading.trailing.equalToSuperview()
+            $0.width.equalToSuperview()
+        })
+        
+//        [topBackgroundView, grayView, myGradientLabel, registerView, foodRecommendButton].forEach({self.view.addSubview($0)})
+        [topBackgroundView, grayView, myGradientLabel, myIngredientsView, foodRecommendButton].forEach({self.contentView.addSubview($0)})
+//        [profileImage, userTasteLabel, foodTasteTestButton].forEach({self.topBackgroundView.addSubview($0)})
+        [nameLabel, typeLabel, typeUnderLine, profileImage, reTestButton].forEach({self.topBackgroundView.addSubview($0)})
         
         topBackgroundView.snp.makeConstraints({
-            $0.top.equalToSuperview()
+            $0.top.equalToSuperview().offset(-20)
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(411)
         })
         
+        nameLabel.snp.makeConstraints({
+            $0.top.equalToSuperview().offset(32)
+            $0.centerX.equalToSuperview()
+        })
+        
+        typeLabel.snp.makeConstraints({
+            $0.top.equalTo(self.nameLabel.snp.bottom).offset(12)
+            $0.centerX.equalToSuperview()
+        })
+        
+        typeUnderLine.snp.makeConstraints({
+            $0.top.equalTo(self.typeLabel.snp.bottom).offset(7)
+            $0.width.equalTo(180)
+            $0.height.equalTo(4)
+            $0.centerX.equalToSuperview()
+        })
+        
         profileImage.snp.makeConstraints({
-            $0.top.equalToSuperview().offset(64)
+            $0.top.equalTo(self.typeUnderLine.snp.bottom).offset(14)
             $0.centerX.equalToSuperview()
             $0.width.equalTo(209)
             $0.height.equalTo(186)
         })
         
-        userTasteLabel.snp.makeConstraints({
-            $0.top.equalTo(self.profileImage.snp.bottom).offset(22)
+        reTestButton.snp.makeConstraints({
+            $0.top.equalTo(self.profileImage.snp.bottom).offset(20)
             $0.centerX.equalToSuperview()
+            $0.width.equalTo(137)
+            $0.height.equalTo(40)
         })
         
-        foodTasteTestButton.snp.makeConstraints({
-            $0.top.equalTo(self.userTasteLabel.snp.bottom).offset(12)
-            $0.centerX.equalToSuperview()
-            $0.width.equalTo(180)
-            $0.height.equalTo(42)
-        })
+//        profileImage.snp.makeConstraints({
+//            $0.top.equalToSuperview().offset(64)
+//            $0.centerX.equalToSuperview()
+//            $0.width.equalTo(209)
+//            $0.height.equalTo(186)
+//        })
+//        
+//        userTasteLabel.snp.makeConstraints({
+//            $0.top.equalTo(self.profileImage.snp.bottom).offset(22)
+//            $0.centerX.equalToSuperview()
+//        })
+        
+//        foodTasteTestButton.snp.makeConstraints({
+//            $0.top.equalTo(self.userTasteLabel.snp.bottom).offset(12)
+//            $0.centerX.equalToSuperview()
+//            $0.width.equalTo(180)
+//            $0.height.equalTo(42)
+//        })
         
         grayView.snp.makeConstraints({
             $0.top.equalTo(self.topBackgroundView.snp.bottom)
@@ -106,32 +206,47 @@ class RecommendFoodTapViewController: UIViewController {
             $0.leading.equalToSuperview().offset(16)
         })
         
-        registerView.snp.makeConstraints({
+//        registerView.snp.makeConstraints({
+//            $0.top.equalTo(self.myGradientLabel.snp.bottom).offset(12)
+//            $0.leading.trailing.equalToSuperview().inset(16)
+//            $0.height.equalTo(190)
+//        })
+        myIngredientsView.snp.makeConstraints({
             $0.top.equalTo(self.myGradientLabel.snp.bottom).offset(12)
             $0.leading.trailing.equalToSuperview().inset(16)
-            $0.height.equalTo(190)
+            $0.height.equalTo(295)
         })
         
+//        foodRecommendButton.snp.makeConstraints({
+//            $0.top.equalTo(self.registerView.snp.bottom).offset(16)
+//            $0.leading.trailing.equalToSuperview().inset(16)
+//            $0.height.equalTo(50)
+//        })
         foodRecommendButton.snp.makeConstraints({
-            $0.top.equalTo(self.registerView.snp.bottom).offset(16)
+            $0.top.equalTo(self.myIngredientsView.snp.bottom).offset(16)
             $0.leading.trailing.equalToSuperview().inset(16)
             $0.height.equalTo(50)
+            $0.bottom.equalToSuperview().offset(-10)
         })
+        
+        self.scrollView.updateContentSize()
     }
     
     private func bind() {
-        foodTasteTestButton.rx.tap
-            .subscribe(onNext: { [weak self] in
-                self?.moveToTasteTest()
-            })
-            .disposed(by: disposeBag)
+//        foodTasteTestButton.rx.tap
+//            .subscribe(onNext: { [weak self] in
+//                self?.moveToTasteTest()
+//            })
+//            .disposed(by: disposeBag)
         
-        registerView.isUserInteractionEnabled = true
-        registerView.registerButton.rx.tap
-            .subscribe (onNext: { [weak self] in
-                self?.moveToFoodRegister()
-            })
-            .disposed(by: disposeBag)
+        
+        
+//        registerView.isUserInteractionEnabled = true
+//        registerView.registerButton.rx.tap
+//            .subscribe (onNext: { [weak self] in
+//                self?.moveToFoodRegister()
+//            })
+//            .disposed(by: disposeBag)
         
         foodRecommendButton.rx.tap
             .subscribe(onNext: { [weak self] in
@@ -146,7 +261,13 @@ class RecommendFoodTapViewController: UIViewController {
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
-    private func moveToFoodRegister() {
+//    private func moveToFoodRegister() {
+//        let vc = RegisterFoodViewController()
+//        vc.modalPresentationStyle = .fullScreen
+//        self.navigationController?.pushViewController(vc, animated: true)
+//    }
+    
+    @objc private func moveToFoodRegister() {
         let vc = RegisterFoodViewController()
         vc.modalPresentationStyle = .fullScreen
         self.navigationController?.pushViewController(vc, animated: true)
