95ac7983d3e477fb6751fefa5754bf2b0f489fa9
keem-hyun <ddiamo997@gmail.com>
2024-09-15 09:26:15 +0900

    [FEAT] 챌린지 뷰 구현 + 친구 검색 기능

---
+++ YoriJori/Presentation/Community/FriendsList/FriendsListModel.swift
@@ -0,0 +1,14 @@
+//
+//  FriendsListModel.swift
+//  YoriJori
+//
+//  Created by 김강현 on 9/14/24.
+//
+
+import UIKit
+
+struct FriendsListModel {
+    let name: String
+    let currentChallenge: String
+    let profileImage: UIImage?
+}
